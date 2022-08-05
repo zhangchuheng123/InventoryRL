3905e8ae920f332dd292e4cc06caeb3b6858e481
Zhang Chuheng <zhangchuheng123@live.com>
2022-08-05 10:10:31 +0800

    exp12

---
+++ exp11/bash.sh
@@ -0,0 +1,2 @@
+CUDA_VISIBLE_DEVICES=0 python sac_discrete.py --config config/exp11_0.yaml &
+CUDA_VISIBLE_DEVICES=1 python sac_discrete.py --config config/exp11_20.yaml &
---
+++ exp12/bash.sh
@@ -0,0 +1,2 @@
+CUDA_VISIBLE_DEVICES=0 python sac_discrete.py --config config/exp12_0.yaml &
+CUDA_VISIBLE_DEVICES=1 python sac_discrete.py --config config/exp12_20.yaml &
