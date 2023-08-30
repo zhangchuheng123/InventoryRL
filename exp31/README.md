caa54056d60a44e5cc883f4af85877bc1b810dce
Zhang Chuheng <zhangchuheng123@live.com>
2023-08-30 16:06:15 +0800

    exp31

--- exp31/README.md
+++ exp31/README.md
@@ -1,1 +1,10 @@
 # InventoryRL
+
+```bash
+conda create -n inv python=3.10
+conda activate inv
+conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
+conda install matplotlib numpy pandas scipy
+conda install -c conda-forge scikit-learn
+pip install pyyaml munch tqdm
+```
--- exp31/additional_experiment.py
+++ exp31/additional_experiment.py
@@ -21,25 +21,25 @@ if __name__ == '__main__':
 
     for P_plus_L in range(2, 14):
 
-    	product_lifetime = P_plus_L // 2 
-    	max_lead_time = P_plus_L - product_lifetime
-    	arrival_prob = list(np.linspace(0, 1, max_lead_time + 2)[1:])
+        product_lifetime = P_plus_L // 2 
+        max_lead_time = P_plus_L - product_lifetime
+        arrival_prob = list(np.linspace(0, 1, max_lead_time + 2)[1:])
 
-    	config.env.product_lifetime = product_lifetime
-    	config.env.max_lead_time = max_lead_time
-    	config.env.arrival_prob = arrival_prob
+        config.env.product_lifetime = product_lifetime
+        config.env.max_lead_time = max_lead_time
+        config.env.arrival_prob = arrival_prob
 
-    	config.config_name = "ppl{}_sacd_ours".format(P_plus_L)
-    	config.budget_estimation_episodes = 20
-	    agent = SacdAgent(config=config)
-	    agent.run()
+        config.config_name = "ppl{}_sacd_ours".format(P_plus_L)
+        config.budget_estimation_episodes = 20
+        agent = SacdAgent(config=config)
+        agent.run()
 
-	    config.config_name = "ppl{}_sacd_bench".format(P_plus_L)
-	    config.budget_estimation_episodes = 1000
-	    agent = SacdAgent(config=config)
-	    agent.run()
+        config.config_name = "ppl{}_sacd_bench".format(P_plus_L)
+        config.budget_estimation_episodes = 1000
+        agent = SacdAgent(config=config)
+        agent.run()
 
-	    config.config_name = "ppl{}_ss".format(P_plus_L)
-	    config.method = "ss_policy"
-	    agent = ClassicalAgent(config=config)
-	    agent.evaluate()
+        config.config_name = "ppl{}_ss".format(P_plus_L)
+        config.method = "ss_policy"
+        agent = ClassicalAgent(config=config)
+        agent.evaluate()
