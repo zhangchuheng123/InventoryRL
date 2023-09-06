cca6ea67450ed1e54530a2a4aa8de428347a7269
Zhang Chuheng <zhangchuheng123@live.com>
2023-09-06 10:00:17 +0800

    add parser

--- exp31/parse_logs.py
+++ exp31/parse_logs.py
@@ -4,39 +4,39 @@ import pandas as pd
 from scipy import interpolate
 from tensorboard.backend.event_processing import event_accumulator
 
-def main(dir_log='logs/', output_path='results/'):
+def main(log_dir='logs/', output_dir='results/'):
 
-	os.makedirs(output_path, exist_ok=True)
+	os.makedirs(output_dir, exist_ok=True)
 
-	process_ss(log_dir, output_path)
-	process_sacd(log_dir, output_path)
+	process_ss(log_dir, output_dir)
+	process_sacd(log_dir, output_dir)
 
-def process_ss(log_dir, output_path, budget=30):
+def process_ss(log_dir, output_dir, budget=30):
 
-	paths = os.listdir(dir_log)
+	paths = os.listdir(log_dir)
 
 	record = []
 	for ppl in [2, 4, 6]:
 		for algo in ['ss_policy']:
 
 			target_path = [item for item in paths if 'ppl{}'.format(ppl) in item and algo in item][0]
-			target_file = os.path.join(dir_log, target_path, 'record', 'stats.csv')
+			target_file = os.path.join(log_dir, target_path, 'record', 'stats.csv')
 
 			df = pd.read_csv(target_file, index_col=0)
 			f = interpolate.interp1d(df['budget'].values, df['cost'].values)
 			record.append(dict(ppl=ppl, budget=budget, cost=f(budget)))
 
-	pd.DataFrame(record).to_csv(os.path.join(target_output_path, 'ss_policy.csv'))
+	pd.DataFrame(record).to_csv(os.path.join(target_output_dir, 'ss_policy.csv'))
 
-def process_sacd(log_dir, output_path):
+def process_sacd(log_dir, output_dir):
 
-	paths = os.listdir(dir_log)
+	paths = os.listdir(log_dir)
 
 	for ppl in [2, 4, 6]:
 		for algo in ['sacd_ours', 'sacd_bench']:
 
 			target_path = [item for item in paths if 'ppl{}'.format(ppl) in item and algo in item][0]
-			target_path = os.path.join(dir_log, target_path, 'summary')
+			target_path = os.path.join(log_dir, target_path, 'summary')
 			target_file = [item for item in os.listdir(target_path) if 'tfevents' in item][0]
 			target_file = os.path.join(target_path, target_file)
 
@@ -51,11 +51,11 @@ def process_sacd(log_dir, output_path):
 
 			pdb.set_trace()
 
-			target_output_path = os.path.join(output_path, 'ppl{}_{}'.format(ppl, algo))
-			os.makedirs(target_output_path, exist_ok=True)
+			target_output_dir = os.path.join(output_dir, 'ppl{}_{}'.format(ppl, algo))
+			os.makedirs(target_output_dir, exist_ok=True)
 
-			pd.DataFrame(ea.Scalars('eval/return_discount')).to_csv(os.path.join(target_output_path, 'return_discount.csv'))
-			pd.DataFrame(ea.Scalars('eval/budget_discount')).to_csv(os.path.join(target_output_path, 'budget_discount.csv'))
+			pd.DataFrame(ea.Scalars('eval/return_discount')).to_csv(os.path.join(target_output_dir, 'return_discount.csv'))
+			pd.DataFrame(ea.Scalars('eval/budget_discount')).to_csv(os.path.join(target_output_dir, 'budget_discount.csv'))
 
 if __name__ == '__main__':
 
