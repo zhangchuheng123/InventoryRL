61c7d92edf17ec5e54d2692cfafb0667fcf8db95
Zhang Chuheng <zhangchuheng123@live.com>
2024-05-21 09:16:18 +0800

    update:

---
+++ exp33/additional_experiment_coeff.py
@@ -0,0 +1,38 @@
+from pathos.multiprocessing import ProcessingPool
+from classical import ClassicalAgent
+from sac_discrete import SacdAgent
+from munch import DefaultMunch
+from functools import partial
+from tqdm import trange
+import numpy as np
+import argparse
+import yaml
+import pdb
+import os 
+
+
+def single_run(init_coeff, config):
+
+    config = config.copy()
+
+    config.config_name = "coeff{}_sacd_ours".format(init_coeff)
+    config.seed = 8000
+    config.env.lost_sale_cost = init_coeff
+
+    agent = SacdAgent(config=config)
+    agent.run()
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', type=str, default=os.path.join('config', 'default.yaml'))
+    args = parser.parse_args()
+
+    with open(args.config) as f:
+        config = yaml.load(f, Loader=yaml.SafeLoader)
+
+    config = DefaultMunch.fromDict(config)
+
+    single_run_partial = partial(single_run, config=config)
+    pool = ProcessingPool(2)
+    pool.map(single_run_partial, [0, 3, 6])
