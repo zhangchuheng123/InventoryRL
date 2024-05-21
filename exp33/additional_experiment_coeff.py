d4c9320f09fe65ca1a2fcae30148a479cf71e53d
Zhang Chuheng <zhangchuheng123@live.com>
2024-05-21 09:30:54 +0800

    update

--- exp33/additional_experiment_coeff.py
+++ exp33/additional_experiment_coeff.py
@@ -15,6 +15,9 @@ def single_run(init_coeff, config):
 
     config = config.copy()
 
+    import pdb 
+    pdb.set_trace()
+
     config.config_name = "coeff{}_sacd_ours".format(init_coeff)
     config.seed = 8000
     config.env.lost_sale_cost = init_coeff
@@ -34,5 +37,8 @@ if __name__ == '__main__':
     config = DefaultMunch.fromDict(config)
 
     single_run_partial = partial(single_run, config=config)
+
+    single_run_partial(0)
+
     pool = ProcessingPool(2)
     pool.map(single_run_partial, [0, 3, 6])
